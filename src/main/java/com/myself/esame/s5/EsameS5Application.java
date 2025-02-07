613590ad5df17b6ca60b32791956362e0cfb33d9
Alessandro Coretti <alessandrocoretti2002@gmail.com>
2025-02-07 20:56:50 +0100

    commit 1

---
+++ src/main/java/com/myself/esame/s5/EsameS5Application.java
@@ -0,0 +1,13 @@
+package com.myself.esame.s5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameS5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameS5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myself/esame/s5/EsameS5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myself.esame.s5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameS5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
